84053229176cfce3dbf0ca27d35f167fc0a19e38
Fasica Robi <fasicasr@gmail.com>
2021-07-25 22:49:56 -0400

    implement function for start and timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,190 +1,93 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
 
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
+var timerElement = document.querySelector(".timer-count");
+let startButton = document.querySelector(".start-button");
+let questionList = document.getElementById('questionList');
+
+
+//pull answer options from html
+
+
+
+
 var timer;
 var timerCount;
 
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
 
-// Array of words the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
+//question list 
+let questions = [
+  {
+    question: "What tag do you use to properly link Javascript to HTML?",
+    choiceA: "<javascript>", 
+    choiceB: "<html>",
+    choiceC: "<script>", 
+    choiceD: "<div>",
+    answer: "C",
+  },
+  {
+    question: "Which <h> tag will get you the biggest font?",
+    choiceA: "<h3>", 
+    choiceB: "<h1>",
+    choiceC: "<h4>",
+    choiceD: "<h6>",
+    answer: "D",
+  },
+  {
+    question: "What kind of values can you store in a boolean?",
+    choiceA: "numbers", 
+    choiceB: "true and false", 
+    choiceC: "strings", 
+    choiceD: "other arrays",
+    answer: "B",
+  },
+  {
+    question: "String values must be place between..",
+    choiceA: "single quotes",  
+    choiceB: "curly brackets",
+    choiceC: "double quotes",
+    choiceD: "both A and D",
+    answer: "D",
+  }, 
+  {
+    question: "Which one of these statments is correct?",
+    choiceA: "CSS is styling sheet language",  
+    choiceB: "JavaScript is a styling Sheet language", 
+    choiceC: "HTML is a programing language",
+    choiceD: "CSS is a programing language",
+    answer: "A",
+  },
+]
 
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
-}
 
-// The startGame function is called when the start button is clicked
-function startGame() {
+
+// Attach event listener to start button to call startQuiz function on click
+startButton.addEventListener("click", startQuiz);
+
+// The startQuiz function is called when the start button is clicked
+function startQuiz() {
   isWin = false;
-  timerCount = 10;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
+  timerCount = 75;
+  startButton.style.display='none'; //button is hidden from page once clicked
+
+  showQuestions();
+  // renderBlanks()
   startTimer()
 }
 
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
+
+//function for questions. To be called so questions can appear
+function showQuestions () {
+ 
+ 
 }
 
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
 
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+// The setTimer function starts and stops the timer
 function startTimer() {
   // Sets timer
   timer = setInterval(function() {
     timerCount--;
     timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
+    
   }, 1000);
 }
 
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
-
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
-
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
-
-// Calls init() so that it fires when page opened
-init();
-
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
-
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
